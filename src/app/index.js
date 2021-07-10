7c954596e48a69a4fc3832f412da37070a4147b4
Maxim Makarov <maxim.makarov14@gmail.com>
2021-07-10 15:58:01 +0300

    Creat draft domains

--- src/app/index.js
+++ src/app/index.js
@@ -1,19 +1,23 @@
+import 'react-native-gesture-handler'
 import React from 'react'
 import { Box, Text, Row, Col } from '@qonsoll/react-native-design'
+import { NavigationContainer } from '@react-navigation/native'
 
 const App = () => (
-  <Row>
-    <Col>
-      <Box bg="black" flex={1}>
-        <Text>Box1</Text>
-      </Box>
-    </Col>
-    <Col>
-      <Box bg="white" flex={1}>
-        <Text>Box2</Text>
-      </Box>
-    </Col>
-  </Row>
+  <NavigationContainer>
+    <Row>
+      <Col>
+        <Box bg="black" flex={1}>
+          <Text>Box1</Text>
+        </Box>
+      </Col>
+      <Col>
+        <Box bg="white" flex={1}>
+          <Text>Box2</Text>
+        </Box>
+      </Col>
+    </Row>
+  </NavigationContainer>
 )
 
 export default App
